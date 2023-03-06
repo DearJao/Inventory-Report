85efadbf5fa30c58abe2c201bb19dd1f3dac2f17
DearJao <jvvr2013@gmail.com>
2023-03-06 16:09:10 -0300

    classe CompleteReport implementada e fix no classe SimpleReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,25 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def stocks_each_company(list):
+        counter_products = Counter(
+            [product["nome_da_empresa"] for product in list]
+        )
+        report = ""
+        for company, qty in counter_products.items():
+            report += f"- {company}: {qty}\n"
+        # print(company, qty)
+        return report
+
+    @classmethod
+    def generate(cls, list):
+        simple_report = super().generate(list)
+        stocks_company = CompleteReport.stocks_each_company(list)
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n {stocks_company}"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -20,9 +20,9 @@ class SimpleReport:
             date.fromisoformat(product["data_de_validade"]) for product in list
         ]
 
-        closest_date = [product for product in data if data >= now]
-        print(closest_date)
-        print(min(closest_date))
+        closest_date = [product for product in data if product >= now]
+        # print(closest_date)
+        # print(min(closest_date))
 
         return min(closest_date)
 
@@ -32,17 +32,18 @@ class SimpleReport:
             product["nome_da_empresa"] for product in list
         )
 
-        return company_produces_most
+        return company_produces_most.most_common()[0][0]
 
+    @staticmethod
+    def generate(list):
+        oldest_fabrication_date = SimpleReport.earliest_manufacturing_date(
+            list
+        )
+        closest_expiration_date = SimpleReport.closest_expiration_date(list)
+        company_most_products = SimpleReport.company_with_most_products(list)
 
-@staticmethod
-def generate(list):
-    oldest_fabrication_date = SimpleReport.earliest_manufacturing_date(list)
-    closest_expiration_date = SimpleReport.closest_expiration_date(list)
-    company_most_products = SimpleReport.company_with_most_products(list)
-
-    return (
-        f"Data de fabricação mais antiga: {oldest_fabrication_date}\n"
-        f"Data de validade mais próxima: {closest_expiration_date}\n"
-        f"Empresa com mais produtos: {company_most_products}"
-    )
+        return (
+            f"Data de fabricação mais antiga: {oldest_fabrication_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_most_products}"
+        )
