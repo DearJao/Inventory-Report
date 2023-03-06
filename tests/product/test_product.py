7d983bf07c02f29f8696c3aa2a50716449d48337
DearJao <jvvr2013@gmail.com>
2023-03-06 12:54:10 -0300

    testa se o inicializador da classe Product funciona

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui ó
+    data = Product(
+        1,
+        "star_industries",
+        "mark_04",
+        "2015",
+        "2017",
+        "sexta-feira",
+        "cuidado possui o poder do sol",
+    )
+
+    assert data.id == 1
+    assert data.nome_da_empresa == "star_industries"
+    assert data.nome_da_empresa == "star_industries"
+    assert data.data_de_fabricacao == "2015"
+    assert data.data_de_validade == "2017"
+    assert data.numero_de_serie == "sexta-feira"
+    assert data.instrucoes_de_armazenamento == "cuidado possui o poder do sol"
